a3fa1a8e3c00add2a7b596c11c78e40d7db36ad9
dimadeloseros1 <dimadeloseros1@gmail.com>
2026-04-26 09:24:00 +1000

    Feat: Implement list & 10 numbers

---
+++ U1172227_Q2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+class listClass {
+
+    void addingNumsList() {
+        list<int> nums;
+
+        nums.push_back(1);
+        nums.push_back(2);
+        nums.push_back(3);
+        nums.push_back(4);
+        nums.push_back(5);
+        nums.push_back(6);
+        nums.push_back(7);
+        nums.push_back(8);
+        nums.push_back(9);
+        nums.push_back(10);
+    }
+
+};
+
+int main() {
+
+}
